8ec3bd6341d5d0be17945d4269d5940eb13dd358
Alaguraj1 <alaguraj259@gmail.com>
2024-01-18 12:13:37 +0530

    signup Section updated

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -4,34 +4,57 @@ import React, { useState, useEffect } from 'react';
 import SideMenuTwo from "../SideMenuTwo";
 import { Table, Button } from 'antd';
 import axios from "axios";
-import { CalendarFilled, CaretDownOutlined, CaretUpOutlined  } from '@ant-design/icons';
+import { CalendarFilled, CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 import { Modal } from 'antd';
 
 function Home() {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [productRate, setProductRate] = useState([])
-    const [goldMaxMin, setGoldMaxMin]  = useState([])
+    const [goldMaxMin, setGoldMaxMin] = useState([])
 
     useEffect(() => {
-        axios.get("https://chatbot.thechennaisilks.com:5575/API/login/GoldRate").then((res) => {
-            setProductRate(res?.data?.results)
-        }).catch((error) => {
-            console.log(error)
+        const token = localStorage.getItem("token");
+        console.log('✌️Token --->', token);
+      
+        axios.get("https://chatbot.thechennaisilks.com:5575/API/login/GoldRate",
+        //  {
+        //   headers: {
+        //     "Authorization": `${token}`
+        //   }
+        // }
+        )
+        .then((response) => {
+          setProductRate(response.data.results);
         })
-    }, [])
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          // You can handle the error in a way that makes sense for your application
+        });
+      
+      }, []);
+      
 
     console.log("productRate", productRate)
 
 
     useEffect(() => {
-        axios.get("https://chatbot.thechennaisilks.com:5575/api/login/MinMaxGoldRate?MonthYear=06-2023")
+        const token = localStorage.getItem("token");
+        console.log('✌️Token --->', token);
+        
+        axios.get("https://chatbot.thechennaisilks.com:5575/api/login/MinMaxGoldRate?MonthYear=06-2023",
+        // {
+        //     headers: {
+        //       "Authorization": `${token}`
+        //     }
+        //   }
+          )
             .then((res) => {
                 setGoldMaxMin(res?.data?.results);
             })
             .catch((error) => {
                 console.error("Axios Error:", error);
-    
+
                 if (error.response) {
                     // The request was made and the server responded with a status code
                     alert("Response Data:", error.response.data);
@@ -44,18 +67,18 @@ function Home() {
                     // Something happened in setting up the request that triggered an Error
                     alert("Error Message:", error.message);
                 }
-    
+
                 // Modal.error({
                 //     title: error?.AxiosError?.message || "Error",
                 //     content: 'Server-side issue',
                 // });
             });
     }, []);
-    
+
 
     console.log("goldMaxMin", goldMaxMin)
 
-    
+
 
     const dataSource = [
         {
@@ -123,8 +146,8 @@ function Home() {
     ];
 
 
-      // modal
-      const showModal = () => {
+    // modal
+    const showModal = () => {
         setIsModalOpen(true);
     };
     const handleOk = () => {
@@ -143,29 +166,29 @@ function Home() {
         <div className="elisc_tm_all_wrap" data-magic-cursor="show" data-enter="fadeInLeft" data-exit="true">
             <SideMenuTwo />
             <div className="elisc_tm_mainpart w-full min-h-[100vh] clear-both float-left pl-[370px]"  >
-                
+
                 <div className="home-container-fluid">
-                <h3 className="chit-details-title">Hi Karthick,  Welcome To Thangam Jewellery</h3>
-                    
+                    <h3 className="chit-details-title">Hi Karthick,  Welcome To Thangam Jewellery</h3>
+
                     <div className="home-container">
-                        
+
                         <div className="home-left">
-                            
+
                             <div className="priceDetails">
-                            <CalendarFilled className="calendor" onClick={showModal} />
+                                <CalendarFilled className="calendor" onClick={showModal} />
                                 {
                                     productRate.map((value) => {
                                         console.log("value", value.RATE1)
                                         return (
                                             <marquee className="product-price">Gold Rate : ₹ {value.RATE1} per GRAM   |  Silver Rate : ₹ {value.RATE2} per GRAM    |  Platinum Rate : ₹ {value.RATE4} per GRAM  </marquee>
-                                        
-                                        //     <div className="scrolling-text">
-                                        //     Gold Rate : ₹ {value.RATE1} per GRAM | Silver Rate : ₹ {value.RATE2} per GRAM | Platinum Rate : ₹ {value.RATE4} per GRAM
-                                        //   </div>
-                                            )
+
+                                            //     <div className="scrolling-text">
+                                            //     Gold Rate : ₹ {value.RATE1} per GRAM | Silver Rate : ₹ {value.RATE2} per GRAM | Platinum Rate : ₹ {value.RATE4} per GRAM
+                                            //   </div>
+                                        )
                                     })
-                                }                            
-                                </div>
+                                }
+                            </div>
 
                             <div className="home-payDue">
                                 <div className="home-table-outer">
@@ -189,26 +212,26 @@ function Home() {
                 </div>
 
                 <Modal title="SEP-GOLD RATE" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} footer={false}>
-                   <div className="gold-stage">
-                    <h6 className="gold-low">LOW <CaretDownOutlined /></h6>
-                    <h6 className="gold-heigh">HIGH <CaretUpOutlined /></h6>
-                    <h6 className="gold-gram">GRAM</h6>
-                   </div>
-                   <div className="gold-price">
-                    {
-                        goldMaxMin.map((value) =>  {
-                            console.log("maxMin rate", value)
-                            return(
-                                <>
-                                 <p  className="gold-minprice">Rs : {value.MINRATE}</p>
-                                <p className="gold-maxprice">Rs : {value.MAXRATE}</p>
-                                <p className="gold-gramprice">1 Gram</p>
-                                </>                              
-                            )
-                        })
-                    }
-                                      
-                   </div>
+                    <div className="gold-stage">
+                        <h6 className="gold-low">LOW <CaretDownOutlined /></h6>
+                        <h6 className="gold-heigh">HIGH <CaretUpOutlined /></h6>
+                        <h6 className="gold-gram">GRAM</h6>
+                    </div>
+                    <div className="gold-price">
+                        {
+                            goldMaxMin.map((value) => {
+                                console.log("maxMin rate", value)
+                                return (
+                                    <>
+                                        <p className="gold-minprice">Rs : {value.MINRATE}</p>
+                                        <p className="gold-maxprice">Rs : {value.MAXRATE}</p>
+                                        <p className="gold-gramprice">1 Gram</p>
+                                    </>
+                                )
+                            })
+                        }
+
+                    </div>
                 </Modal>
 
             </div>
--- src/component/Signup/Singup.js
+++ src/component/Signup/Singup.js
@@ -1,6 +1,6 @@
 import "./Signup.css"
 import React, { useState } from 'react';
-import { Form, Input, Button, Checkbox,} from 'antd';
+import { Form, Input, Button, Checkbox, message } from 'antd';
 import { Link } from 'react-router-dom/dist';
 import { EyeOutlined, EyeInvisibleOutlined, UserOutlined, UnlockOutlined, MailOutlined, PhoneOutlined } from '@ant-design/icons'
 import SideMenu from "../SideMenu";
@@ -17,13 +17,14 @@ function Signup() {
     const togglePasswordVisibility = () => {
         setPasswordVisible(!passwordVisible);
     };
+    const [messageApi, contextHolder] = message.useMessage();
 
     const initialValues = {
         IMEINUM: 1,
         APPCODE: 1,
         APPVERSION: 1,
         FCMID: 1,
-      };
+    };
 
     const onFinish = (values) => {
         console.log('Form values:', values);
@@ -41,11 +42,20 @@ function Signup() {
             .then((res) => {
                 console.log(res);
 
-                if (res?.data?.Success === 1) {
+                if (res?.data?.results[0]?.Success === 1) {
+                    messageApi.open({
+                        type: 'success',
+                        content: "successfully registred",
+                    });
                     navigate('/');
                 } else {
                     // Handle unsuccessful response
                     error();
+
+                    messageApi.open({
+                        type: 'error',
+                        content: res?.data?.results[0]?.Msg,
+                    });
                 }
             })
             .catch((error) => {
@@ -93,124 +103,127 @@ function Signup() {
     };
 
     return (
-        <div className="elisc_tm_all_wrap" data-magic-cursor="show" data-enter="fadeInLeft" data-exit="true">
+        <>
             <SideMenu />
-            <div className="elisc_tm_mainpart w-full min-h-[100vh] clear-both float-left pl-[370px]">
+            <div className="elisc_tm_all_wrap" data-magic-cursor="show" data-enter="fadeInLeft" data-exit="true">
+                {contextHolder}
 
-                <div className='container-signup'>
-                    <div className="signup-left">
-                        <h1 className="signup-title">WELCOME</h1>
-                        <p className="login-subTitle">User Signup</p>
-                        <div style={{ marginTop: "20px" }}>
-                            <Form
-                                form={form}
-                                name="signup-form"
-                                initialValues={initialValues}
-                                onFinish={onFinish}
-                                onFinishFailed={onFinishFailed}
-                                layout="vertical"
-                                className="login-form"
-                            >
-                                <Form.Item
-                                    name="NAME"
-                                    label="Name"
-                                    rules={[
-                                        {
-                                            whitespace: true,
-                                            message: "Please enter your name!",
-                                        },
-                                    ]}
+                <div className="elisc_tm_mainpart w-full min-h-[100vh] clear-both float-left pl-[370px]">
+
+                    <div className='container-signup'>
+                        <div className="signup-left">
+                            <h1 className="signup-title">WELCOME</h1>
+                            <p className="login-subTitle">User Signup</p>
+                            <div style={{ marginTop: "20px" }}>
+                                <Form
+                                    form={form}
+                                    name="signup-form"
+                                    initialValues={initialValues}
+                                    onFinish={onFinish}
+                                    onFinishFailed={onFinishFailed}
+                                    layout="vertical"
+                                    className="login-form"
                                 >
-                                    <div className="login-input-warrper">
-                                        < UserOutlined className="login-input-icon" />
-                                        <Input className="login-input-style" />
-                                    </div>
-                                </Form.Item>
+                                    <Form.Item
+                                        name="NAME"
+                                        label="Name"
+                                        rules={[
+                                            {
+                                                whitespace: true,
+                                                message: "Please enter your name!",
+                                            },
+                                        ]}
+                                    >
+                                        <div className="login-input-warrper">
+                                            < UserOutlined className="login-input-icon" />
+                                            <Input className="login-input-style" />
+                                        </div>
+                                    </Form.Item>
 
-                                <Form.Item
-                                    name="MAILID"
-                                    label="Email"
-                                    rules={[
-                                        {
-                                            type: "email",
-                                            message: "The input is not a valid email!"
-                                        },
-                                        {
-                                            message: "Please enter your email!"
-                                        }
-                                    ]}
-                                >
-                                    <div className="login-input-warrper">
-                                        <MailOutlined className="login-input-icon" />
-                                        <Input className="login-input-style" />
-                                    </div>
-                                </Form.Item>
+                                    <Form.Item
+                                        name="MAILID"
+                                        label="Email"
+                                        rules={[
+                                            {
+                                                type: "email",
+                                                message: "The input is not a valid email!"
+                                            },
+                                            {
+                                                message: "Please enter your email!"
+                                            }
+                                        ]}
+                                    >
+                                        <div className="login-input-warrper">
+                                            <MailOutlined className="login-input-icon" />
+                                            <Input className="login-input-style" />
+                                        </div>
+                                    </Form.Item>
 
-                                <Form.Item
-                                    name="USERNAME"
-                                    label="Mobile Number"
-                                    style={{ fontSize: "18px !important" }}
-                                    rules={[
-                                        {
-                                            required: false,
-                                            message: 'Please enter your mobile number!',
-                                        },
-                                        {
-                                            pattern: /^[0-9]{10}$/,
-                                            message: 'Mobile number must be a 10-digit number!',
-                                        },
-                                    ]}
-                                >
-                                    <div className="login-input-warrper">
-                                        <PhoneOutlined className="login-input-icon" />
-                                        <Input
-                                            type="tel"
-                                            className="login-input-style"
-                                            maxLength={10}
-                                        />
-                                    </div>
-                                </Form.Item>
+                                    <Form.Item
+                                        name="USERNAME"
+                                        label="Mobile Number"
+                                        style={{ fontSize: "18px !important" }}
+                                        rules={[
+                                            {
+                                                required: false,
+                                                message: 'Please enter your mobile number!',
+                                            },
+                                            {
+                                                pattern: /^[0-9]{10}$/,
+                                                message: 'Mobile number must be a 10-digit number!',
+                                            },
+                                        ]}
+                                    >
+                                        <div className="login-input-warrper">
+                                            <PhoneOutlined className="login-input-icon" />
+                                            <Input
+                                                type="tel"
+                                                className="login-input-style"
+                                                maxLength={10}
+                                            />
+                                        </div>
+                                    </Form.Item>
 
-                                <Form.Item
-                                    name="PASSWORD"
-                                    label="Password"
-                                    style={{ fontSize: "18px !important" }}
-                                    rules={[
-                                        {
-                                            message: 'Please enter your password!',
-                                        },
-                                    ]}
-                                >
-                                    <div className="login-input-wrapper">
-                                        <UnlockOutlined className="login-input-icon" />
-                                        <Input
-                                            type={passwordVisible ? 'text' : 'password'}
-                                            className="login-input-style"
-                                        />
-                                        {passwordVisible ? (
-                                            <EyeOutlined onClick={togglePasswordVisibility} className="eyeIcon" />
-                                        ) : (
-                                            <EyeInvisibleOutlined onClick={togglePasswordVisibility} className="eyeIcon" />
-                                        )}
-                                    </div>
-                                </Form.Item>
+                                    <Form.Item
+                                        name="PASSWORD"
+                                        label="Password"
+                                        style={{ fontSize: "18px !important" }}
+                                        rules={[
+                                            {
+                                                message: 'Please enter your password!',
+                                            },
+                                        ]}
+                                    >
+                                        <div className="login-input-wrapper">
+                                            <UnlockOutlined className="login-input-icon" />
+                                            <Input
+                                                type={passwordVisible ? 'text' : 'password'}
+                                                className="login-input-style"
+                                            />
+                                            {passwordVisible ? (
+                                                <EyeOutlined onClick={togglePasswordVisibility} className="eyeIcon" />
+                                            ) : (
+                                                <EyeInvisibleOutlined onClick={togglePasswordVisibility} className="eyeIcon" />
+                                            )}
+                                        </div>
+                                    </Form.Item>
 
-                                <Form.Item name="IMEINUM" initialValue={initialValues.IMEINUM} hidden>
-                                    <Input />
-                                </Form.Item>
+                                    <Form.Item name="IMEINUM" initialValue={initialValues.IMEINUM} hidden>
+                                        <Input />
+                                    </Form.Item>
 
-                                <Form.Item name="APPCODE" initialValue={initialValues.APCODE}  hidden>
-                                    <Input />
-                                </Form.Item>
+                                    <Form.Item name="APPCODE" initialValue={initialValues.APCODE} hidden>
+                                        <Input />
+                                    </Form.Item>
 
-                                <Form.Item name="APPVERSION"  initialValue={initialValues.APPVERSION} hidden>
-                                    <Input />
-                                </Form.Item>
+                                    <Form.Item name="APPVERSION" initialValue={initialValues.APPVERSION} hidden>
+                                        <Input />
+                                    </Form.Item>
 
-                                <Form.Item name="FCMID" initialValue={initialValues.FCMID} hidden>
-                                    <Input />
-                                </Form.Item>
-                                {/* <Form.Item
+                                    <Form.Item name="FCMID" initialValue={initialValues.FCMID} hidden>
+                                        <Input />
+                                    </Form.Item>
+                                    {/* <Form.Item
                                     name="confirmPassword"
                                     label="Confirm Password"
                                     dependencies={['password-signup']}
@@ -231,35 +244,37 @@ function Signup() {
                                     <Input className="login-input-style"/>
                                 </Form.Item> */}
 
-                                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                                    <Form.Item name="remember" valuePropName="checked">
-                                        <Checkbox>Remember me</Checkbox>
+                                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Form.Item name="remember" valuePropName="checked">
+                                            <Checkbox>Remember me</Checkbox>
+                                        </Form.Item>
+                                        {/* <p style={{textDecoration:'underline'}}>Forgot Password</p> */}
+                                    </div>
+
+                                    <Form.Item>
+                                        <Button
+                                            type="primary"
+                                            htmlType="submit"
+                                            style={{ background: "#9a2526" }}
+                                            size="large"
+                                        >
+                                            Sign Up
+                                        </Button>
                                     </Form.Item>
-                                    {/* <p style={{textDecoration:'underline'}}>Forgot Password</p> */}
-                                </div>
-
-                                <Form.Item>
-                                    <Button
-                                        type="primary"
-                                        htmlType="submit"
-                                        style={{ background: "#9a2526" }}
-                                        size="large"
-                                    >
-                                        Sign Up
-                                    </Button>
-                                </Form.Item>
-                            </Form>
-                            <p>Already user ? <Link to="/login" style={{ textDecoration: 'underline' }}>Login</Link></p>
+                                </Form>
+                                <p>Already user ? <Link to="/login" style={{ textDecoration: 'underline' }}>Login</Link></p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="signup-outer">
-                        <img src="assets/img/bg-1.png" alt="side-modal"  className="login-side-img" />
-                    </div>
+                        <div className="signup-outer">
+                            <img src="assets/img/bg-1.png" alt="side-modal" className="login-side-img" />
+                        </div>
 
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
+
     );
 }
 
